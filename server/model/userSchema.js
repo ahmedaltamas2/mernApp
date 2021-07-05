8eff538219e5c77445ccbf5c01c2ea80eb86ef73
altamas ahmed <ahmedaltamas2@gmail.com>
2021-07-06 02:49:34 +0530

    hash password using bcrypt

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongooose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongooose.Schema({
     name: {
@@ -27,6 +28,28 @@ const userSchema = new mongooose.Schema({
     }
 })
 
+
+
+//hash the password
+//this is a middleware before 'save' in auth i.e line c
+userSchema.pre('save', async function(next){
+    // console.log("hi");
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,12);
+        this.cpassword = await bcrypt.hash(this.cpassword,12);
+
+    }
+    next();
+
+});
+
+
 const User = mongooose.model('USER', userSchema);
 
 module.exports = User;
+
+
+
+
+
+
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
 
 router.get('/', (req, res) => {
     res.send(`Hello world from the server rotuer js`);
@@ -10,7 +12,7 @@ require('../db/conn');
 const User = require("../model/userSchema");
 
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
   
     const {name,email,phone,work,password,cpassword} = req.body;
 
@@ -18,18 +20,27 @@ router.post('/register', (req, res) => {
         return res.status(422).json({error: "please fill the full form"});
     } 
 
-    User.findOne({email : email})
-        .then((userExist) => {
-            if(userExist){
-                return res.status(422).json({ error : "Email already Exist"});
-            }
+    try{
+        const userExist = await  User.findOne({email : email});
 
+        if(userExist){
+            return res.status(422).json({ error : "Email already Exist"});
+        }else if(password != cpassword){
+            return res.status(422).json({ error : "Email already Exist"});
+
+        }else{
             const user = new User({name,email,phone,work,password,cpassword});
 
-            user.save().then(() => {
-                res.status(201).json({ message : "user registered successfully"});
-            }).catch((err) => res.status(500).json({error: "Fialed to registered"}));
-        }).catch( err => {console.log(err);});
+            //line c
+            await user.save();
+
+            res.status(201).json({ message : "user registered successfully"});
+        }
+
+    }catch(err){
+        console.log(err);
+    }
+
 });
 
 router.post('/signin', async (req,res) => {
@@ -42,15 +53,26 @@ router.post('/signin', async (req,res) => {
 
         const userLogin = await User.findOne({email:email});
 
-        console.log(userLogin);
+        // console.log(userLogin);
 
+        if(userLogin){
+            const isMatch = await bcrypt.compare(password,userLogin.password);
 
-        if(!userLogin){
-            res.status(400).json({error : "user error"})
+            if(!isMatch){
+                res.status(400).json({error : "Invalid Credentials"})
+            }else{
+                res.json({message : "user signin successfully"});
+    
+            }
         }else{
-            res.json({message : "user signin successfully"});
+            res.status(400).json({error : "Invalid Credentials"});
 
         }
+
+      
+
+
+       
 
     }catch(err){
         console.log(err);
