8993f78cad5afca99ebfd1a79989e598a4fb5480
altamas ahmed <ahmedaltamas2@gmail.com>
2021-07-05 16:46:37 +0530

    connect Db using promises

--- server/router/auth.js
+++ server/router/auth.js
@@ -5,9 +5,31 @@ router.get('/', (req, res) => {
     res.send(`Hello world from the server rotuer js`);
 });
 
+
+require('../db/conn');
+const User = require("../model/userSchema");
+
+
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({ message: req.body });
+  
+    const {name,email,phone,work,password,cpassword} = req.body;
+
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error: "please fill the full form"});
+    } 
+
+    User.findOne({email : email})
+        .then((userExist) => {
+            if(userExist){
+                return res.status(422).json({ error : "Email already Exist"});
+            }
+
+            const user = new User({name,email,phone,work,password,cpassword});
+
+            user.save().then(() => {
+                res.status(201).json({ message : "user registered successfully"});
+            }).catch((err) => res.status(500).json({error: "Fialed to registered"}));
+        }).catch( err => {console.log(err);});
 });
 
 module.exports = router;
