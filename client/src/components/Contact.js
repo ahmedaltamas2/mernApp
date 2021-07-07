1322d43859ffa030a137d9ede46db8eecc38abae
altamas ahmed <ahmedaltamas2@gmail.com>
2021-07-07 17:36:22 +0530

    create contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,12 +1,45 @@
 import React from 'react'
-
-const Contact = () => {
+import { NavLink } from 'react-router-dom'
+function Login() {
     return (
-        <div>
-            <p>Welcome @ contact</p>
-            <h1>This is my MERN App</h1>
+        <>
+       <div class="login-form">
+    <form  method="post">
+        <h2 class="text-center">GET IN TOUCH</h2>   
+        <div class="form-group">
+        	<div class="input-group">
+                <div class="input-group-prepend">
+                                       
+                </div>
+                <input type="text" className="form-control"  placeholder="Your Name" required="required"/>				
+            </div>
         </div>
+		<div class="form-group">
+            <div class="input-group" >
+                <div class="input-group-prepend">
+                                       
+                </div>
+                <input type="password" className="form-control"  placeholder="Your Email" required="required"/>				
+            </div>
+        </div> 
+
+         <div class="form-group">
+        	<div class="input-group">
+                <div class="input-group-prepend">
+                                       
+                </div>
+                <input type="text"  className="form-control-contact"  placeholder="Message" required="required"/>				
+            </div>
+        </div>  
+     
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary login-btn btn-block">Send Message</button>
+        </div>
+      
+    </form>
+</div>
+        </>
     )
 }
 
-export default Contact
+export default Login
