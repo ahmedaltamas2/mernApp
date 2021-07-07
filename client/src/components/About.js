6a0ab40854da14bbbcb7409b87ac904ccf694ac9
altamas ahmed <ahmedaltamas2@gmail.com>
2021-07-08 02:14:16 +0530

    add about me page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import dp from '../images/dp.jpg';
 
-const About = () => {
+function About() {
     return (
         <div>
-            <p>Welcome @ About us</p>
-            <h1>This is my MERN App</h1>
-        </div>
+          <h2>About Me</h2>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	
+	
+
+	<div class="carousel-inner">		
+		<div class="carousel-item active">
+			<div class="img-box"><img src={dp} alt=""/></div>
+			<p class="testimonial">I am a B.tech IT student at NSUT WEST CAMPUS.I completed my 12th from HHFS New Delhi</p>
+			<p class="overview"><b>Altamas Ahmed</b> MERN developer <a href="#"></a></p>
+			
+		</div>
+         </div> 
+         </div>
+         </div>
     )
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const Home = () => {
+
+function Home() {
+    const mystyle = {
+        color: "black",
+        backgroundColor: "#ffdc12",
+        textAlign: "center",
+        padding: "200px",
+        fontFamily: "sans-serif", 
+        height: "100vh"
+      };
     return (
-        <div>
-            <p>Welcome</p>
-            <h1>This is my MERN App</h1>
-        </div>
+        < >
+            <div style={mystyle}>
+                <h1>
+                WELCOME EVERYONE
+                </h1>
+                <p>KEEP YOUR CONTACT INFORMATION HERE</p>
+               
+            </div>
+            
+         
+        </>
     )
 }
 
